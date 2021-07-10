42cff075accc1365b4611a9df00bb1c7dbe815a9
Aditya Kasibhatla <dev.kasibhatla@gmail.com>
2021-07-10 14:42:12 +0530

    Allow node integration

--- application/main.js
+++ application/main.js
@@ -1,16 +1,27 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
+const url = require('url')
+const shell = require('electron').shell
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
 	webPreferences: {
-	      preload: path.join(__dirname, 'src/preload.js')
+	      preload: path.join(__dirname, 'src/preload.js'),
+        nodeIntegration: true,
+        contextIsolation: false
     }
   })
 
-  win.loadFile('src/index.html')
+  // win.loadFile('src/index.html')
+  win.loadURL(url.format({
+    pathname: path.join(__dirname,'src/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  //todo: add other window listeners here
 }
 
 app.whenReady().then(() => {
