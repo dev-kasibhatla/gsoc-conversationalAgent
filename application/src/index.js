af02a1d4cd80522056cac159f1f33b73ea11f19c
Aditya Kasibhatla <dev.kasibhatla@gmail.com>
2021-07-31 19:34:07 +0530

    initialise app if not already initialised

--- application/src/index.js
+++ application/src/index.js
@@ -1,5 +1,22 @@
+
 const electron = require('electron')
 const path = require('path')
-const BrowserWindow = electron.remote.BrowserWindow
+// const {AppState} = require("../main");
+const {initialiseApp} = require("./main");
+//logv already imported in nav.js
+// const {logv} = require("./common");
+// const BrowserWindow = electron.remote.BrowserWindow
 
 console.log('index.js was initialised')
+
+/*
+if(!AppState.appInitialised) {
+    logv('app initialised:');
+    logv(!AppState.appInitialised);
+    initialiseApp();
+    AppState.appInitialised = true;
+    logv('app initialised:');
+    logv(!AppState.appInitialised);
+}*/
+
+initialiseApp();
