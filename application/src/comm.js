eb908f7537969b2c5f43d49f89e55cc8319ce619
Aditya Kasibhatla <dev.kasibhatla@gmail.com>
2021-07-31 19:32:18 +0530

    Methods to start agents, and send requests

--- application/src/comm.js
+++ application/src/comm.js
@@ -3,6 +3,8 @@
  */
 
 const exec = require('child_process').exec;
+const request = require('request');
+const {logv} = require("./common");
 
 var statusRCRemoteServer = false, statusLauncher = false;
 
@@ -33,16 +35,98 @@ function initBasics() {
         logv(output);
         //todo: how foolproof is this?
         if(output.includes("ok")) {
-            logv('recremote server started successfully');
+            logv('rcremote server started successfully');
+            statusRCRemoteServer = true;
         }
     });
 
     logv('starting launcher');
+    //todo: start listening to the strean
     execute(runLauncher,(output)=>{
+        logv('output of starting launcher');
         logv(output);
         //todo: how foolproof is this?
 
     });
+    statusLauncher = true;
 }
 
-initBasics();
+async function sendPostRequest(urlPack,body) {
+    logv('sending a post request');
+    logv(urlPack);
+    //expecting a URLProvider object for url
+    let completeUrl = `${urlPack.url}`;
+    let options = {
+        'method': 'POST',
+        'url': completeUrl,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    };
+    try {
+        //todo: errors are not getting caught
+        let response = await request(options);
+        logv("response:");
+        logv(response);
+        return new Response(response,false);
+    }catch(e) {
+        logv('post request failed');
+        logv(e);
+        return new Response(e,true);
+    }
+}
+
+class URLPack {
+    //should be a const
+    static baseUrl = 'http://localhost:5002/webhooks/rest/webhook/';
+    port = '0';
+    url = "";
+
+    constructor(url, port) {
+        this.port = port;
+        this.url = url;
+    }
+}
+
+class URLProvider {
+    static messageUrl = new URLPack(URLPack.baseUrl,'5002',);
+}
+
+class Response {
+    body="";
+    statusCode="";
+    isError;
+    error;
+    response;
+    constructor(response, error) {
+        if(error === false) {
+            if(response.body === undefined){
+                // logv('response is an error');
+                error=true;
+            }else {
+                logv('response is not an error');
+                this.response = response;
+                this.body = response.body;
+                logv(this.body);
+            }
+        }
+        if(error === true){
+            logv('response is an error');
+            this.error = response;
+            logv(error);
+        }
+        this.isError=true;
+    }
+
+
+}
+
+module.exports = {
+    execute,
+    initBasics,
+    sendPostRequest,
+    URLProvider,
+    URLPack,
+    Response
+}
